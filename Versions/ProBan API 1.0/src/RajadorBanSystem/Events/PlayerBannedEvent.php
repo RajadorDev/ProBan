5538cf4d4beee7f85fa3bc38ec791681060b51e3
RajadorDev <125204182+RajadorDev@users.noreply.github.com>
2023-05-20 12:53:23 -0300

    Add files via upload

---
+++ Versions/ProBan API 1.0/src/RajadorBanSystem/Events/PlayerBannedEvent.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace RajadorBanSystem\Events;
+
+/*
+  
+  Rajador Developer
+
+  ▒█▀▀█ ░█▀▀█ ░░░▒█ ░█▀▀█ ▒█▀▀▄ ▒█▀▀▀█ ▒█▀▀█ 
+  ▒█▄▄▀ ▒█▄▄█ ░▄░▒█ ▒█▄▄█ ▒█░▒█ ▒█░░▒█ ▒█▄▄▀ 
+  ▒█░▒█ ▒█░▒█ ▒█▄▄█ ▒█░▒█ ▒█▄▄▀ ▒█▄▄▄█ ▒█░▒█
+
+  GitHub: https://github.com/RajadorDev
+
+  Discord: Rajador#7070
+
+
+*/
+
+
+class PlayerBannedEvent extends PunishEvent 
+{
+	
+	public static $handlerList = null;
+	
+	public function getEventId() : int 
+	{
+		return \RajadorBanSystem\ProBan::PLAYER_BANNED_EVENT;
+	}
+	
+}
