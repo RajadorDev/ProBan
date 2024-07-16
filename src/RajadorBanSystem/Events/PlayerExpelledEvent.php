46463532068393b7b66702298e17a611a0ca25f2
RajadorDev <125204182+RajadorDev@users.noreply.github.com>
2024-07-16 18:29:25 -0300

    unzipgit

---
+++ src/RajadorBanSystem/Events/PlayerExpelledEvent.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace RajadorBanSystem\Events;
+
+/*
+  
+  Rajador Developer
+
+  ▒█▀▀█ ░█▀▀█ ░░░▒█ ░█▀▀█ ▒█▀▀▄ ▒█▀▀▀█ ▒█▀▀█ 
+  ▒█▄▄▀ ▒█▄▄█ ░▄░▒█ ▒█▄▄█ ▒█░▒█ ▒█░░▒█ ▒█▄▄▀ 
+  ▒█░▒█ ▒█░▒█ ▒█▄▄█ ▒█░▒█ ▒█▄▄▀ ▒█▄▄▄█ ▒█░▒█
+
+  GitHub: https://github.com/RajadorDev
+
+  Discord: Rajador#7070
+
+
+*/
+
+class PlayerExpelledEvent extends PunishEvent 
+{
+	
+	public static $handlerList = null;
+	
+	public function getEventId() : int 
+	{
+		return \RajadorBanSystem\ProBan::PLAYER_EXPELL_EVENT;
+	}
+	
+}
