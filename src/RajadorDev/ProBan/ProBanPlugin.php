75fc107562223250ad1ce300f5ba23a5540a29ad
RajadorDev <rajadordeveloper@gmail.com>
2024-12-30 12:37:21 -0300

    Main

---
+++ src/RajadorDev/ProBan/ProBanPlugin.php
@@ -0,0 +1,27 @@
+<?php
+
+declare (strict_types=1);
+
+/***
+ *   
+ * Rajador Developer
+ * 
+ * ▒█▀▀█ ░█▀▀█ ░░░▒█ ░█▀▀█ ▒█▀▀▄ ▒█▀▀▀█ ▒█▀▀█ 
+ * ▒█▄▄▀ ▒█▄▄█ ░▄░▒█ ▒█▄▄█ ▒█░▒█ ▒█░░▒█ ▒█▄▄▀ 
+ * ▒█░▒█ ▒█░▒█ ▒█▄▄█ ▒█░▒█ ▒█▄▄▀ ▒█▄▄▄█ ▒█░▒█
+ * 
+ * GitHub: https://github.com/RajadorDev
+ * 
+ * Discord: rajadortv
+ * 
+ * 
+**/
+
+namespace RajadorDev\ProBan;
+
+use pocketmine\plugin\PluginBase;
+
+final class ProBanPlugin extends PluginBase 
+{
+    
+}
